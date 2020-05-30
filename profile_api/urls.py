a5dd12e2f6431a955f72594f820b77bfa63cf297
Priyanka <piyu.pp1596@gmail.com>
2020-05-31 00:16:40 +0530

    commit

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -6,7 +6,10 @@ from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
 router.register('hello-viewset',views.HelloViewSet,base_name='hello-viewset')
+router.register('profile',views.UserProfileViewSet)
+
 urlpatterns=[
     url(r'hello-view/',views.HelloApiView.as_view()),
     url(r'',include(router.urls))
+
 ]
--- profile_api/views.py
+++ profile_api/views.py
@@ -4,7 +4,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from profile_api import serializers
 from rest_framework import viewsets
-
+from profile_api import models
+from rest_framework.authentication import TokenAuthentication
+from profile_api import permissions
 
 class HelloApiView(APIView):
     serializer_class=serializers.HelloSerializer
@@ -84,3 +86,11 @@ class HelloViewSet(viewsets.ViewSet):
 
     def destroy(self,request,pk=None):
         return Response({'http_method':'DELETE'})
+
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class=serializers.UserProfileSerializer
+    queryset=models.UserProfile.objects.all()
+    authentication_classes=(TokenAuthentication,)
+    permission_classes=(permissions.updateownprofile,)
